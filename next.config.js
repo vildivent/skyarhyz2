03dcf283ec61f66e9a1b225ab8715465146d4d57
vildivent <vildivent@mail.ru>
2023-11-22 19:08:02 +0300

    changed config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,22 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+const { env } = await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    domains: [
+      env.FILE_UPLOADER_DOMAIN,
+      "cdn.discordapp.com",
+      "lh3.googleusercontent.com",
+      "avatars.yandex.net",
+      "localhost",
+    ],
+  },
+  experimental: {
+    scrollRestoration: true,
+  },
+};
 
 export default config;
