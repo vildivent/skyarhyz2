ea0555ac3e030ede95cb465d11598af8edc8eadb
vildivent <vildivent@mail.ru>
2023-11-24 00:39:39 +0300

    added logo

---
+++ src/shared/ui/Logo.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import type { DetailedHTMLProps, HTMLAttributes } from "react";
+
+export function Logo({
+  className = "",
+  ...props
+}: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>) {
+  return (
+    <div className={`font-h text-4xl font-bold ${className}`} {...props}>
+      <span className="text-primary">Sky</span>
+      <span>Arhyz</span>
+    </div>
+  );
+}
+export function LogoNavbar() {
+  return (
+    <div className="ml-2 flex flex-1 items-center md:ml-10">
+      <Link href="/">
+        <Logo className="!text-3xl" />
+      </Link>
+    </div>
+  );
+}
+export function LogoAuth() {
+  return (
+    <div className="mb-6 flex">
+      <Link href="/">
+        <Logo />
+      </Link>
+    </div>
+  );
+}
