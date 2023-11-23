ffdd509b60b1bba4567bb6f84bfd2b961567b2f3
vildivent <vildivent@mail.ru>
2023-11-24 00:07:20 +0300

    changed fonts behaviour

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -2,11 +2,11 @@ import { Alegreya, Mulish } from "next/font/google";
 
 export const alegrea = Alegreya({
   subsets: ["latin", "cyrillic"],
-  display: "swap",
+  display: "block",
   variable: "--font-alegrea",
 });
 export const mulish = Mulish({
   subsets: ["latin", "cyrillic"],
-  display: "swap",
+  display: "block",
   variable: "--font-mulish",
 });
