b30647f4014a4824af4d4a45f6c6e42a012fb3b5
vildivent <vildivent@mail.ru>
2023-11-26 16:13:11 +0300

    addded fonts & colors to tw

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,20 @@
 import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       fontFamily: {
-        h: ["var(--font-alegrea)", ...fontFamily.serif],
-        p: ["var(--font-mulish)", ...fontFamily.sans],
+        h: ["var(--font-alegrea)"],
+        p: ["var(--font-mulish)"],
+      },
+      colors: {
+        darkgray: "#1e1e1e",
+        grayish: "#292929",
+        lightgray: "#444444",
+        smoke: "#dddddd",
+        primary: "rgb(2 132 199)",
+        secondary: "rgb(3 105 161)",
       },
     },
   },
