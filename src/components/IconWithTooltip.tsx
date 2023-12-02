8866065d4556bfc46607207afdd6e2af1a241115
vildivent <vildivent@mail.ru>
2023-12-02 18:11:40 +0300

    added tooltips

---
+++ src/components/IconWithTooltip.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { Tooltip } from "react-tooltip";
+
+type IconWithTooltipProps = {
+  id: string;
+  icon: React.ReactNode;
+  tooltip: string;
+  className?: string;
+};
+export default function IconWithTooltip({
+  id,
+  icon,
+  tooltip,
+  className = "",
+}: IconWithTooltipProps) {
+  return (
+    <>
+      <div
+        className={`z-0 flex h-10 w-10 shrink-0 cursor-pointer items-center justify-center ${className}`}
+        data-tooltip-id={"tooltip-" + id}
+        data-tooltip-content={tooltip}
+      >
+        {icon}
+      </div>
+      <Tooltip id={"tooltip-" + id} place="bottom" className="border" noArrow />
+    </>
+  );
+}
